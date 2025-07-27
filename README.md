095cf23183e4c2b3afb9ebfc83c8920f33ea1bd0
friskobar <alexandre.ctff@gmail.com>
2025-07-27 11:51:20 -0300

    i blacked out for a while and now the vulkan tutorial uses raii, ts is the new branch

--- README.md
+++
@@ -1,3 +0,0 @@
-Renderer with vulkan and c++ to demonstrate my knowledge.
-
-will probably do something with it in the future
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,5 @@
-#include <iostream>
-#include <stdexcept>
-#include <cstdlib>
-
-#include "Application.hpp"
 
 int main(){
-    Application app;
-
-    try {
-        app.run();
-    } catch(const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    };
-
-    return EXIT_SUCCESS;
+    
+    return 1;
 }
